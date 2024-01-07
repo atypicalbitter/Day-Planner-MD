425cc038946de121e760c2d7da366cb8780ecde9
Matt Dudman <mattdudman95@googlemail.com>
2024-01-07 12:12:11 +0000

    If/Else statement added in to give user the ability to apply past, presnet or future class to the current hour.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     }
 
     $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+    if (hour < currentHour) {
+        textArea.addClass("past");
+    } else if (hour === currentHour) {
+        textArea.addClass("present");
+    } else {
+        textArea.addClass("future");
+    }
 });
